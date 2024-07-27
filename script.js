9a02308744b04bd5b8f6a4cc426d16787a7f072e
Kyler Long <kylerlong24@gmail.com>
2024-07-27 18:30:11 -0400

    Create function to generate a random color and create click event to call this function.

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-//Create a div evelement with a class of cell and append to sketch-container
-//Style the cell in css 
+//Create a div evelement with a class of cell and append to sketch-container within for loop to create 16x16 grid
+//Create toDraw function with eventListeners for mouse entering and leaving, resulting in random color generation 
 
 const sketchContainer = document.querySelector(".sketch-container");
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 16*16; i++) {
     const cellDiv = document.createElement("Div");
     sketchContainer.appendChild(cellDiv);
     cellDiv.setAttribute("class", "cell");
+
+    const getRandomColor = () => {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        cellDiv.style.backgroundColor = "#" + randomColor; 
+    }
+    
+    cellDiv.addEventListener("click", getRandomColor);
 }
