26d964fdc2c3e496172669235ba4f8905c43e988
Kyler Long <kylerlong24@gmail.com>
2024-07-29 18:33:24 -0400

    Add slider function that alters the number of squares in the sketch container based on user input value.
    Move reset button outside of the loop to increase
    performance.
    
    Alter reset button to create a node list of cells
    from create cells function and change each cell
    to white background color when clicked

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
-//Create a div evelement with a class of cell and append to sketch-container within for loop to create 16x16 grid
-//Create toDraw function with eventListeners for mouse entering and leaving, resulting in random color generation 
+//**resizing feature with slider**
+//create eventListener for slider that will store the value in a variable,
+//and call createCells with new value
 
 const sketchContainer = document.querySelector(".sketch-container");
 const reset = document.querySelector(".reset");
+const sliderNumber = document.querySelector(".number-slider");
+const sliderDisplay = document.querySelector(".slider-value");
+sliderDisplay.textContent = 16;
 
-for (let i = 0; i < 16*16; i++) {
-    const cellDiv = document.createElement("Div");
-    sketchContainer.appendChild(cellDiv);
-    cellDiv.setAttribute("class", "cell");
+const createCells = (numberOfCells) => {
+    sketchContainer.innerHTML = "";
 
-    const getRandomColor = () => {
-        const randomColor = Math.floor(Math.random()*16777215).toString(16);
-        cellDiv.style.backgroundColor = "#" + randomColor; 
+    //calculate the size of each cell based on the container size and number of cells
+    const containerSize = 256; 
+    const cellSize = containerSize / numberOfCells; 
+    document.documentElement.style.setProperty("--cell-size", `${cellSize}px`);
+
+    for (let i = 0; i < numberOfCells*numberOfCells; i++) {
+        const cellDiv = document.createElement("div");
+        sketchContainer.appendChild(cellDiv);
+        cellDiv.setAttribute("class", "cell");
+
+        const getRandomColor = () => {
+            const randomColor = Math.floor(Math.random()*16777215).toString(16);
+            cellDiv.style.backgroundColor = "#" + randomColor; 
+        }
+        
+        cellDiv.addEventListener("mouseover", getRandomColor);
     }
-    
-    cellDiv.addEventListener("mouseover", getRandomColor);
-
-    const clearColor = () => {
-        cellDiv.style.backgroundColor = "#f1f1f1";
-    }
-    
-    reset.addEventListener("click", clearColor);
 }
+
+createCells(16);
+
+sliderNumber.addEventListener("input", (e) => {
+    const sliderValue = sliderNumber.value;
+    sliderDisplay.textContent = sliderValue;
+    createCells(sliderValue);
+});
+
+
+reset.addEventListener("click", (e) => {
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => {
+        cell.style.backgroundColor = "#f1f1f1";
+    })
+});
 
 
 
